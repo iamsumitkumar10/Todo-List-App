a7facffb8ae8a15b7198bfeb301ea84c17c04d51
Sumit Kumar <108218318+iamsumitkumar10@users.noreply.github.com>
2024-09-19 23:49:08 +0530

    add delete function

---
+++ src/components/deletelist.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, task: 'Learn React' },
+    { id: 2, task: 'Build a Todo App' },
+    { id: 3, task: 'Master JavaScript' },
+  ]);
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
